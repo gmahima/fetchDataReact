71faa8cf251cfbd2492e426218a659f455937115
Mahima Gangavarapu <39587007+gmahima@users.noreply.github.com>
2019-08-05 22:13:00 +0530

    fetch api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import React, { Component } from 'react';
-import './App.css';
-import SearchForm from './Components/SearchForm';
-import GifList from './Components/GifList';
+import React, { Component } from "react";
+import "./App.css";
+import SearchForm from "./Components/SearchForm";
+import GifList from "./Components/GifList";
 
 export default class App extends Component {
-  
-  constructor() {
-    super();
-  } 
-
-  render() { 
+  state = {
+    gifs: []
+  };
+  componentDidMount() {
+    fetch("http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC")
+      .then(response => response.json())
+      .then(responseData => this.setState({ gifs: responseData.data }))
+      .catch(error => {
+        console.log("error fetching and parsing data", error);
+      });
+  }
+  render() {
+    console.log(this.state.gifs);
     return (
       <div>
         <div className="main-header">
           <div className="inner">
             <h1 className="main-title">GifSearch</h1>
-            <SearchForm />      
-          </div>   
-        </div>    
+            <SearchForm />
+          </div>
+        </div>
         <div className="main-content">
           <GifList />
         </div>
